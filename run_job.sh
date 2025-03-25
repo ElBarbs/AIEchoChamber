c5f36a1ff691f97b7a84c091db8d8a3572daa722
Louis B. <47404526+ElBarbs@users.noreply.github.com>
2025-03-25 10:17:46 -0400

    Updated copy function.

--- run_job.sh
+++ run_job.sh
@@ -8,11 +8,11 @@
 module load python/3.12.4
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
-cp /home/lbarbier/projects/def-vigliens/lbarbier/AIEchoChamber/files $SLURM_TMPDIR/AIEchoChamber
-cp /home/lbarbier/projects/def-vigliens/lbarbier/AIEchoChamber/huggingface $SLURM_TMPDIR/AIEchoChamber
+cp -r /home/lbarbier/projects/def-vigliens/lbarbier/AIEchoChamber/files $SLURM_TMPDIR/AIEchoChamber
+cp -r /home/lbarbier/projects/def-vigliens/lbarbier/AIEchoChamber/huggingface $SLURM_TMPDIR/AIEchoChamber
 cd $SLURM_TMPDIR/AIEchoChamber/files
 pip install --no-index --upgrade pip
 pip install --no-index -r requirements.txt
 pip install compel-2.0.3-py3-none-any.whl
 python main.py --input "flowers.jpg"
-cp $SLURM_TMPDIR/AIEchoChamber/files/output /home/lbarbier/projects/def-vigliens/lbarbier/AIEchoChamber/output
+cp -r $SLURM_TMPDIR/AIEchoChamber/files/output /home/lbarbier/projects/def-vigliens/lbarbier/AIEchoChamber/output
