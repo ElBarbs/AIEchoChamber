a2fe7ad35048855ada7d4eb83e5361626337bd1f
Louis B. <47404526+ElBarbs@users.noreply.github.com>
2025-03-25 13:45:28 -0400

    Added no-index to compel install.

--- run_job.sh
+++ run_job.sh
@@ -2,8 +2,8 @@
 #SBATCH --account=def-vigliens
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=6
-#SBATCH --mem=20000M
-#SBATCH --time=00:05:00
+#SBATCH --mem=10000M
+#SBATCH --time=00:10:00
 
 module load python/3.12.4
 virtualenv --no-download $SLURM_TMPDIR/env
@@ -15,6 +15,6 @@ wait
 cd $SLURM_TMPDIR/AIEchoChamber/files
 pip install --no-index --upgrade pip
 pip install --no-index -r requirements.txt
-pip install compel-2.0.3-py3-none-any.whl
+pip install --no-index compel-2.0.3-py3-none-any.whl
 python main.py --input "flowers.jpg"
 cp -r $SLURM_TMPDIR/AIEchoChamber/files/output /project/def-vigliens/lbarbier/AIEchoChamber/output
